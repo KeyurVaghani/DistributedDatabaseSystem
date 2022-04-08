581528d3709165718e6ceb7cd6a47c410e1a9fd9
Gandhi Rajan Mahendran <gn479247@dal.ca>
2022-04-08 01:06:44 -0300

    - updated Rest Template V3

--- src/main/java/com/dpgten/distributeddb/test/TestRestTemplateController.java
+++ src/main/java/com/dpgten/distributeddb/test/TestRestTemplateController.java
@@ -1,52 +1,40 @@
 package com.dpgten.distributeddb.test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-
 @RestController(value = "/rest")
 public class TestRestTemplateController {
 
+    @Autowired
+    RestTemplate restTemplate;
     private String baseURL;
-
-    @Value("${application.instance.type}")
+//    @Value("${application.instance.type}")
     private Integer instanceType;
-
-    @Value("${api.host.baseurl.instanceOne}")
+//    @Value("${api.host.baseurl.instanceOne}")
     private String baseURLOne;
-
-    @Value("${api.host.baseurl.instanceTwo}")
+//    @Value("${api.host.baseurl.instanceTwo}")
     private String baseURLTwo;
-
     private String localURL = "http://localhost:8087";
 
-    TestRestTemplateController(){
+    TestRestTemplateController(@Value("${application.instance.type}") Integer instanceType,
+                               @Value("${api.host.baseurl.instanceOne}") String baseURLOne,
+                               @Value("${api.host.baseurl.instanceTwo}") String baseURLTwo) {
         System.out.println("We are in the instance number--> " + instanceType);
-        if(instanceType ==1){
+        if (instanceType == 1) {
             baseURL = baseURLTwo;
-        }
-        else if(instanceType == 2){
+        } else if (instanceType == 2) {
             baseURL = baseURLOne;
-        }
-        else{
+        } else {
             baseURL = localURL;
         }
         System.out.println("Base URL is--> " + baseURL);
     }
-
-    @Autowired
-    RestTemplate restTemplate;
 
 //    @RequestMapping(value = "/template/products", method = RequestMethod.POST)
 //    public String createProducts(@RequestBody Product product) {
@@ -61,14 +49,14 @@ public class TestRestTemplateController {
     @RequestMapping(value = "/template/get", method = RequestMethod.GET)
     public String getProducts() {
 //        RestTemplate restTemplate = new RestTemplate();
-        String fooResourceUrl  =  baseURL +"/test/get";
+        String fooResourceUrl = baseURL + "/test/get";
         ResponseEntity<String> response
-                = restTemplate.getForEntity(fooResourceUrl , String.class);
+                = restTemplate.getForEntity(fooResourceUrl, String.class);
 //    Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
 //        ObjectMapper mapper = new ObjectMapper();
 //        JsonNode root = null;
 //        String result = "";
-        String responseStr ="";
+        String responseStr = "";
         try {
             responseStr = response.getBody();
             System.out.println(responseStr);
@@ -76,7 +64,7 @@ public class TestRestTemplateController {
 //            result = root.toString();
 //        } catch (JsonProcessingException e) {
 //            e.printStackTrace();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 //        JsonNode name = root.path("name");
