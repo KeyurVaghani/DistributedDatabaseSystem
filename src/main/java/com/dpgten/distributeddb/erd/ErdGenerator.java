077b380a78de1a26414712faf923e540360f996c
Sai Chinthirla <sz836105@dal.ca>
2022-04-10 13:39:42 -0300

    erd generation changes

--- src/main/java/com/dpgten/distributeddb/erd/ErdGenerator.java
+++ src/main/java/com/dpgten/distributeddb/erd/ErdGenerator.java
@@ -1,16 +1,18 @@
 package com.dpgten.distributeddb.erd;
 
 import ch.qos.logback.classic.db.names.ColumnName;
+import io.swagger.v3.oas.models.links.Link;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class ErdGenerator {
-    private final String schema = "schema";
+    private final String schema = "src\\main\\resources\\schema\\";
     private final String primarykey = "Sno";
     private final String delimeter1 = "\\|";
     private final String DELIMETER_COMMA = ",";
@@ -20,78 +22,89 @@ public class ErdGenerator {
     public Scanner scanner;
 
 
-    public void firstErd() throws IOException {
-        File obj = getFileResource(schema);
-        scanner = new Scanner(System.in);
-        System.out.println("Please select database: ");
-        String requiredDatabase = scanner.next();
-        String[] databaseList = obj.list();
-        StringBuilder builder = new StringBuilder();
-//        ArrayList<String> databaseCheckerList = new ArrayList<>();
-        for (int i = 0; i < databaseList.length; i++) {
-//            if (!databaseCheckerList.contains(databaseList[i])) {
-            String selectedDatabase = null;
-            if (requiredDatabase == databaseList[i]) {
-                selectedDatabase = requiredDatabase;
-
-//                builder.append("CREATE DATABASE " + databaseList[i] + SEMI_COLON + NEW_LINE);
-//                databaseCheckerList.add(databaseList[i]);
-//            }
-                System.out.println("Database: " + selectedDatabase + "selected.");
-            } else {
-                System.out.println("Database not found please re enter the database name.");
-            }
-//            String column = schema + "//" + databaseList[i];
-            String column = schema + "//" + selectedDatabase;
-            File tableList = getFileResource(column);
-            String[] tableIterate = tableList.list();
-            for (int j = 0; j < tableIterate.length; j++) {
-                InputStream table = getFileFromResourceAsStream(column + "//" + tableIterate[j]);
-                String tableName = tableIterate[j];
-                tableName = tableName.substring(0, tableName.length() - 4);
-                InputStreamReader inputStreamReader = new InputStreamReader(table, StandardCharsets.UTF_8);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String primaryKey = bufferedReader.readLine().split(delimeter1)[1];
-                //add logic to add foreign key inside create table
-                String foreignKey = bufferedReader.readLine();
-                String[] columnList = bufferedReader.readLine().split(delimeter1);
-                ArrayList<String> columnNames = new ArrayList<String>();
-                ArrayList<String> columnDataTypes = new ArrayList<String>();
-                for (int index = 1; index < columnList.length; index++) {
-                    String[] temp = columnList[index].split(DELIMETER_COMMA);
-                    columnNames.add(temp[0].trim());
-                    columnDataTypes.add(temp[1].trim());
+    public void generateRequiredERD() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a existing database name : ");
+        String selectedDatabase = sc.nextLine();
+        File file = new File(schema + "\\" + selectedDatabase);
+        StringBuffer finalStringBuffer = new StringBuffer();
+        if (file.exists()) {
+            File[] tableList = file.listFiles();
+            if (tableList.length != 0) {
+                for (File table: tableList) {
+                    StringBuffer stringBuffer = new StringBuffer();
+                    BufferedReader reader = new BufferedReader(new FileReader(table));
+                    String tableName = table.getName().replace(".txt", "");
+                    stringBuffer.append("TABLE NAME : ").append(tableName).append(NEW_LINE);
+                    String line;
+                    List<String> columnList = null;
+                    String[] columnArray = null;
+                    while ((line = reader.readLine()) != null) {
+                        if (line.startsWith("Column")) {
+                            columnArray = line.split(Pattern.quote("|"));
+                        }
+                    }
+                    String[] dataType = new String[columnArray.length];
+                    for (int i=0; i < dataType.length; i++) {
+                        dataType[i] = (String.format("%s", columnArray[i]));
+                    }
+                    String columns = String.join(", ", Arrays.copyOfRange(dataType, 1, dataType.length));
+                    stringBuffer.append("COLUMNS in table : ").append(columns).append(NEW_LINE);
+                    finalStringBuffer.append(stringBuffer.toString()).append(NEW_LINE);
                 }
-               // String prefix = "";
-//                builder.append("CREATE TABLE " + tableName + "( ");
-                System.out.println("============ Table: "+tableName+" ===============");
-                for (int index = 0; index < columnNames.size(); index++) {
-//                    builder.append(prefix);
-//                    builder.append(columnNames.get(index)).append(" ").append(columnDataTypes.get(index));
-                    System.out.println(columnNames + " " + columnDataTypes);
-                 //   prefix = ", ";
-                }
-                System.out.println("===============END OF TABLE=======================");
-                System.out.println("Primary key: "+"//add pk variable");
-                System.out.println("Foregin key: "+"//add fk variable");
-                builder.append(");\n");
-                //String line;
-//                while ((line = bufferedReader.readLine()) != null) {
-//                    String[] rowData = line.split(delimeter1);
-//                    String insertQuery = "INSERT INTO " + tableName + " VALUES(";
-//                    prefix = "";
-//                    for (int index = 1; index < rowData.length; index++) {
-//                        insertQuery += prefix;
-//                        insertQuery += rowData[index];
-//                        prefix = ",";
-//                    }
-//                    insertQuery += ");\n";
-//                    builder.append(insertQuery);
-//                }
+                finalStringBuffer.append("RelationShip between tables : ").append(NEW_LINE);
+                for (File table: tableList) {
+                    StringBuffer realtionBuffer = new StringBuffer();
+                    BufferedReader reader = new BufferedReader(new FileReader(table));
+                    String line;
+                    String[] foreignKey = null;
+                    List<String> foreignKeyList = new ArrayList<>();
+                    while ((line = reader.readLine()) != null) {
+                        if (line.startsWith("foreign_key")) {
+                            foreignKey = line.split(Pattern.quote("|"));
+                            List<String> relation = new LinkedList<>(Arrays.asList(foreignKey));
+                            relation.remove(0);
+                            List<String> finalRealtion = null;
+                            for (String s: relation) {
+                                String[] arr = s.split(",");
+                                finalRealtion = new LinkedList<>(Arrays.asList(arr));
+                            }
+                            for (String relTale: finalRealtion) {
+                                String fkTable = relTale.split(Pattern.quote("->"))[0].split(Pattern.quote("$$"))[1];
+                                foreignKeyList.add(fkTable);
+                            }
+                        }
+                    }
 
+                    if (!foreignKeyList.isEmpty()) {
+                        realtionBuffer.append(table.getName().replace(".txt", ""));
+                        realtionBuffer.append(foreignKeyList.get(0));
+                        realtionBuffer.append(" one to many relationship -  ").append(",");
+                        for (int i=0; i<foreignKeyList.size(); i++) {
+                            realtionBuffer.append(foreignKeyList.get(i)).append(",");
+
+                        }
+                        finalStringBuffer.append(realtionBuffer).append(NEW_LINE);
+                    }
+                }
+                writeToFile(finalStringBuffer, selectedDatabase);
             }
+        } else {
+            System.out.println("Please Create The Database First As The Database Does Not Exist");
         }
-        //System.out.println(builder);
+    }
+
+    private void writeToFile(StringBuffer finalStringBuffer, String selectedDatabase) throws IOException {
+        String generatedErdLocation = "src\\main\\resources\\ERD";
+        File file = new File(generatedErdLocation);
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        File dumpPath = new File(generatedErdLocation + "\\" + selectedDatabase + ".txt");
+        dumpPath.createNewFile();
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(dumpPath, true));
+        bufferedWriter.append(finalStringBuffer.toString());
+        bufferedWriter.flush();
 
     }
 
@@ -127,7 +140,7 @@ public class ErdGenerator {
 
     public static void main(String[] args) throws IOException {
         ErdGenerator erd= new ErdGenerator();
-        erd.firstErd();
+        erd.generateRequiredERD();
     }
 
 }
