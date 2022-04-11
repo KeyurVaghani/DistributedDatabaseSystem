d36e1a518c441a760d59a1863660d1c2466ec02d
Prakrut Suthar <pr298346@dal.ca>
2022-04-10 22:09:05 -0300

    Final edits for analytics in AnalyticsOutput

--- src/main/java/com/dpgten/distributeddb/analytics/AnalyticsOutput.java
+++ src/main/java/com/dpgten/distributeddb/analytics/AnalyticsOutput.java
@@ -2,12 +2,12 @@ package com.dpgten.distributeddb.analytics;
 
 public class AnalyticsOutput {
     public AnalyticsOutput(){
-        System.out.println("user abc submitted "+  DatabaseAnalytics.TOTAL_QUERY_COUNT+ "queries for DB1 running on Virtual Machine 1");
-        System.out.println("user abc updated "+  DatabaseAnalytics.UPDATE_QUERY_COUNT+ "queries for DB1 running on Virtual Machine 1");
-        System.out.println("user abc create "+  DatabaseAnalytics.CREATE_QUERY_COUNT+ "queries for DB1 running on Virtual Machine 1");
-        System.out.println("user abc used select "+  DatabaseAnalytics.SELECT_QUERY_COUNT+ "queries for DB1 running on Virtual Machine 1");
-        System.out.println("user abc used insert "+  DatabaseAnalytics.INSERT_QUERY_COUNT+ "queries for DB1 running on Virtual Machine 1");
-        System.out.println("user abc used database "+  DatabaseAnalytics.DATABASE_QUERY_COUNT+ "queries for DB1 running on Virtual Machine 1");
+        System.out.println("user abc submitted "+  DatabaseAnalytics.TOTAL_QUERY_COUNT+ " queries for DB1 running on Virtual Machine 1");
+        System.out.println("user abc updated "+  DatabaseAnalytics.UPDATE_QUERY_COUNT+ " queries for DB1 running on Virtual Machine 1");
+        System.out.println("user abc create "+  DatabaseAnalytics.CREATE_QUERY_COUNT+ " queries for DB1 running on Virtual Machine 1");
+        System.out.println("user abc select "+  DatabaseAnalytics.SELECT_QUERY_COUNT+ " queries for DB1 running on Virtual Machine 1");
+        System.out.println("user abc insert "+  DatabaseAnalytics.INSERT_QUERY_COUNT+ " queries for DB1 running on Virtual Machine 1");
+        System.out.println("user abc database "+  DatabaseAnalytics.DATABASE_QUERY_COUNT+ " queries for DB1 running on Virtual Machine 1");
 
     }
 }
